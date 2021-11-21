9fb615fc7cc2615e6cd0e4fb02952eca3d0028e1
EvgeniyK96 <93151413+EvgeniyK96@users.noreply.github.com>
2021-11-21 23:26:53 +0500

    Update search.js

--- js/search.js
+++ js/search.js
@@ -49,8 +49,8 @@ const search = function () {
         localStorage.setItem('goods', JSON.stringify(array))
 
         
-        if (window.location.pathname !== "goods.html") {
-          window.location.href = "goods.html"
+        if (window.location.pathname !== "/goods.html") {
+          window.location.href = "/goods.html"
         } else {
           renderGoods(array)
         }
